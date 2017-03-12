68ee53f44823c28a3e8bf7cfe324a880fd1d2aeb
MohamedBassem <medoox240@gmail.com>
2017-03-12 18:42:40 +0200

    Adding package doc

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*Package godge is used to build online judges for workshops and meetups. You
+define the tasks of the workshop along with the tests for each task. Users
+submit their submission with the command line client which then get judged and
+displayed on the scoreboard.
+
+An example for testing that the users are able to print to stdout and use flags:
+https://github.com/MohamedBassem/godge/blob/master/example/example.go
+*/
+package godge
